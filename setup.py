fb81689611cfb393ec1cbd33f727f888338cd181
Adrien Renaud <adrien.renaud@capgemini.com>
2017-10-11 21:43:42 +0000

    Adding setup and license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyjave',
+  packages = ['pyjave'], # this must be the same as the name above
+  version = '0.1',
+  description = 'PLotly and Pandas tools',
+  author = 'Adrien Renaud',
+  author_email = 'adrien.renaud@gmail.com',
+  url = 'https://github.com/adrienrenaud/pyjave', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['pandas', 'plotly'], # arbitrary keywords
+  classifiers = [],
+)

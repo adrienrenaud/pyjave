53ebe27355099a89b178092f7b891f2030088b08
Adrien Renaud <adrien.renaud@capgemini.com>
2018-10-02 19:33:45 +0000

    Add pd.DataFrame.printnb

--- pyjave/pandastools.py
+++ pyjave/pandastools.py
@@ -1,5 +1,6 @@
 from . import plotlytools
 import pandas as pd
+from IPython.display import display
 import plotly.offline as py
 
 try:
@@ -12,4 +13,11 @@ def _jhist(self, bins=None, range=None):
     fig = plotlytools.histo_w_error(data, bins, range)
     return py.iplot(fig)
 
+def _printnb(self, name='DataFrame', n=2):
+    print(f'\n::: {name}')
+    display(self.head(n))
+    print(f'Shape: {self.shape}')
+
+
+pd.DataFrame.printnb = _printnb
 pd.Series.jhist = _jhist
